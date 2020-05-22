b217615e7281c4a2ef992d2114ce3077b778fe17
Dhananjay Ashok <dhananjay.ashok99@gmail.com>
2020-05-22 11:35:12 +0530

    Updated Equations 31-51 and deleted csv files

--- Main.py
+++ Main.py
@@ -6,4 +6,4 @@ func_set = ['add', 'mul', 'sub', 'div', 'exp', 'sqrt', 'sin', 'cos', 'tan']
 
 trainer = Trainer(path="data//", save=True, load=True)
 gp = GPLearnSystem(func_set=func_set)
-print(trainer.predict_equations(gp, eqs=2, input_range=(-200, 200)))
+print(trainer.predict_equations(gp, eqs=51, input_range=(-200, 200)))
